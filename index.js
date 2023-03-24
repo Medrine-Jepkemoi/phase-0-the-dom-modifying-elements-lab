15e2cc2327a8066276c219f32345461fc3aa9bfe
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-24 23:40:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Write your code here!
 
+//Removing DOM node 'main#main
 const main = document.querySelector("#main");
 main.remove();
+
+//Adding an h1 element with an id victory under the body and adding text to it
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.append(newHeader);
+console.log(newHeader)
+
+
+
